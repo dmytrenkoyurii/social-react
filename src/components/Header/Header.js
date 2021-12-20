103d373107ee7ec97a229370379f674a4506ca17
dmytrenkoyurii <dmitrenko.yura@gmail.com>
2021-12-20 21:23:02 +0200

    18 - add Dialogs page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,9 @@ import s from './Header.module.css';
 
 const Header = () => {
     return <header className={s.header}>
-        <img src='https://cdn-icons.flaticon.com/png/512/719/premium/719731.png?token=exp=1640006881~hmac=27ca4ce673ab1d4f7cdb9c2482bb1cb9' />
+        <img src='https://cdn-icons.flaticon.com/png/512/719/premium/719731.png?token=exp=1640026407~hmac=7bb9ade3b498d8a3d76ff217fd96a3fc'
+        alt='logo'
+        />
     </header>
 }
 
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,7 +4,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div  className={s.user}>
                 <img className={s.user_cover}
                  src='https://images.unsplash.com/photo-1517292987719-0369a794ec0f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80'
