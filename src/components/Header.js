89fe420b5335383908156777d12b9d52e72ba3ac
dmytrenkoyurii <dmitrenko.yura@gmail.com>
2021-12-20 16:13:25 +0200

    14. start project

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from './Header.module.css';
+
+const Header = () => {
+    return <header className={s.header}>
+        <img src='https://cdn-icons.flaticon.com/png/512/719/premium/719731.png?token=exp=1640006881~hmac=27ca4ce673ab1d4f7cdb9c2482bb1cb9' />
+    </header>
+}
+
+export default Header;
+
