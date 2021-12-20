286e95af0a445d67782eef8bdf8e58385209e406
dmytrenkoyurii <dmitrenko.yura@gmail.com>
2021-12-20 22:24:39 +0200

    19 - route, browser-router

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,9 +3,12 @@ import s from './Header.module.css';
 
 const Header = () => {
     return <header className={s.header}>
-        <img src='https://cdn-icons.flaticon.com/png/512/719/premium/719731.png?token=exp=1640026407~hmac=7bb9ade3b498d8a3d76ff217fd96a3fc'
-        alt='logo'
-        />
+        <a href="/">
+            <img src='https://cdn-icons.flaticon.com/png/512/719/premium/719731.png?token=exp=1640026407~hmac=7bb9ade3b498d8a3d76ff217fd96a3fc'
+            alt='logo'
+            />
+        </a>
+
     </header>
 }
 
