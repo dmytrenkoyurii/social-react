5b11e196ce5bd56b8a412244e887a4407356f0a0
dmytrenkoyurii <dmitrenko.yura@gmail.com>
2021-12-20 17:59:53 +0200

    15 - folders structure, new components

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import s from './Profile.module.css';
+import MyPosts from "./MyPosts/MyPosts";
+
+const Profile = () => {
+    return (
+        <div className={s.content}>
+            <div  className={s.user}>
+                <img className={s.user_cover}
+                 src='https://images.unsplash.com/photo-1517292987719-0369a794ec0f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80'
+                 alt="user cover"/>
+            </div>
+            <div className={s.user_info}>
+                <img className={s.user_img} src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="user photo"/>
+                <div>
+                    <h3>Yurii Dmytrenko</h3>
+                    <p>Year of Birth: 1993</p>
+                    <p>Date of Birth: 10 December</p>
+                    <p>Country: Ukraine</p>
+                    <p>City: Poltava</p>
+                </div>
+            </div>
+            <MyPosts />
+        </div>
+    )
+}
+
+export default Profile;
