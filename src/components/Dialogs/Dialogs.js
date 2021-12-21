f9a792f23b979cd90828e7eb666be487c25f9e78
dmytrenkoyurii <dmitrenko.yura@gmail.com>
2021-12-21 23:11:11 +0200

    21 - add Dialogs markup

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -3,8 +3,35 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return(
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Yurii
+                </div>
+                <div className={s.dialog}>
+                    Vlad
+                </div>
+                <div className={s.dialog}>
+                    Ihor
+                </div>
+                <div className={s.dialog}>
+                    Stas
+                </div>
+                <div className={s.dialog}>
+                    Vitaliy
+                </div>
+                <div className={s.dialog}>
+                    Andriy
+                </div>
+                <div className={s.dialog}>
+                    Roman
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi!</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>What are you working on?</div>
+            </div>
         </div>
     )
 }
