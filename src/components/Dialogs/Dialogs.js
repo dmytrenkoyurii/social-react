007e581196c355ab893b57eb01d54fa7900fe456
dmytrenkoyurii <dmitrenko.yura@gmail.com>
2021-12-21 23:26:26 +0200

    22 - NavLink for Dialogs

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,30 +1,31 @@
 import React from 'react'
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
-    return(
+    return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 <div className={s.dialog + ' ' + s.active}>
-                    Yurii
+                    <NavLink to="/dialogs/1">Yurii</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Vlad
+                    <NavLink to="/dialogs/2">Vlad</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Ihor
+                    <NavLink to="/dialogs/3">Ihor</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Stas
+                    <NavLink to="/dialogs/4">Stas</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Vitaliy
+                    <NavLink to="/dialogs/5">Vitaliy</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Andriy
+                    <NavLink to="/dialogs/6">Andriy</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Roman
+                    <NavLink to="/dialogs/7">Roman</NavLink>
                 </div>
             </div>
             <div className={s.messages}>
